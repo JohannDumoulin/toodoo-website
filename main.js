feafe0c6387fc9617cc04a4419150c796310fa36
thejojo974 <johanndumoulin734@gmail.com>
2021-06-06 10:48:36 +0200

    Dev Section World container

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.scss'
 
 import logoUrl from './assets/img/logo.png'
 document.getElementById('logo-img').src = logoUrl
@@ -8,3 +8,15 @@ document.getElementById('intro-img').src = introUrl
 
 import scrollDownUrl from './assets/img/scroll-down.png'
 document.getElementById('scroll-down-img').src = scrollDownUrl
+
+import enchantedForestUrl from './assets/img/enchanted-forest.png'
+document.getElementById('enchanted-forest-img').src = enchantedForestUrl
+
+import submarineWorldUrl from './assets/img/submarine-world.png'
+document.getElementById('submarine-world-img').src = submarineWorldUrl
+
+import bigIcebergUrl from './assets/img/big-iceberg.png'
+document.getElementById('big-iceberg-img').src = bigIcebergUrl
+
+import sandDunesUrl from './assets/img/sand-dunes.png'
+document.getElementById('sand-dunes-img').src = sandDunesUrl
